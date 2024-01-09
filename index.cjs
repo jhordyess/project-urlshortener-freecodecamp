c287cceb01cde2b08a9b6dcb773163efceda361a
jhordyess <dev@jhordyess.com>
2024-01-09 13:35:18 +0000

    Add body-parser and fix start script

--- index.cjs
+++ index.cjs
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 
 // Basic Configuration
@@ -8,6 +9,10 @@ const port = process.env.PORT || 3000
 
 app.use(cors())
 
+// Help to parse the body of the request from x-www-form-urlencoded to JSON
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Serve static assets
 app.use('/public', express.static(`${process.cwd()}/public`))
 
 app.get('/', (_, res) => {
